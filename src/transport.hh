91ccd168b056f86176bdb871b692542b22b952d1
Volker Birk <vb@pep-project.org>
2021-09-01 18:35:33 +0200

    generate header files for enums

--- src/transport.hh
+++ src/transport.hh
@@ -39,6 +39,20 @@ namespace pEp {
 
             PEP_transport_id get_id() { return id; }
 
+            // the signal_ function register signal handlers
+            // they must be called while a transport is running, otherwise they
+            // throw std::logic_error in case the transport is shut down
+
+            void
+                signal_statuschange(std::function(void(PEP_transport_status_code)));
+
+            void
+                signal_sendto_result(std::function(void(std::string,
+                                std::string, PEP_transport_status_code)));
+
+            void
+                signal_incoming_message(std::function(void(PEP_transport_status_code)));
+
             virtual void configure(const Config& config);
             virtual void startup(callback_execution cbe = PEP_cbe_polling);
             virtual void shutdown();
@@ -46,25 +60,11 @@ namespace pEp {
             virtual void sendto(pEp::Message& msg);
             virtual Message recvnext();
 
-            // the signal_ function register signal handlers
-            // they must be called while a transport is running, otherwise they
-            // throw std::logic_error in case the transport is shut down
-
-            virtual void
-                signal_statuschange(std::function(void(PEP_transport_status_code)));
-
-            virtual void
-                signal_sendto_result(std::function(void(std::string,
-                                std::string, PEP_transport_status_code)));
-
-            virtual void
-                signal_incoming_message(std::function(void(PEP_transport_status_code)));
-
             virtual bool shortmsg_supported();
             virtual bool longmsg_supported();
             virtual bool longmsg_formatted_supported();
             virtual PEP_text_format native_text_format();
     };
-};
+}
 
 #endif // __TRANSPORT_HH__
