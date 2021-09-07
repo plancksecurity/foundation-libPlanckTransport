b6de48b1960a31385036bf2f3581a86741f18942
Dirk Zimmermann <dz@pep.security>
2021-09-07 16:05:21 +0200

    IPS-9 New xcode project

---
+++ xcode/PEPSCTPTransport/PEPSCTPTransport/PEPSCTPTransport.h
@@ -0,0 +1,12 @@
+//
+//  PEPSCTPTransport.h
+//  PEPSCTPTransport
+//
+//  Created by Dirk Zimmermann on 07.09.21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPSCTPTransport : NSObject
+
+@end
---
+++ xcode/PEPSCTPTransport/PEPSCTPTransport/PEPSCTPTransport.m
@@ -0,0 +1,12 @@
+//
+//  PEPSCTPTransport.m
+//  PEPSCTPTransport
+//
+//  Created by Dirk Zimmermann on 07.09.21.
+//
+
+#import "PEPSCTPTransport.h"
+
+@implementation PEPSCTPTransport
+
+@end
