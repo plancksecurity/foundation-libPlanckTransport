57ec24e061717b357c176a85a5b0b581270f30bf
Dirk Zimmermann <dz@pep.security>
2021-09-08 09:52:46 +0200

    IPS-9 Add a test target again

---
+++ xcode/PEPSCTPTransport/PEPSCTPTransportTests/PEPSCTPTransportTests.m
@@ -0,0 +1,36 @@
+//
+//  PEPSCTPTransportTests.m
+//  PEPSCTPTransportTests
+//
+//  Created by Dirk Zimmermann on 08.09.21.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PEPSCTPTransportTests : XCTestCase
+
+@end
+
+@implementation PEPSCTPTransportTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
