65c9c4ca17c2293aad7490279490f93c1683718a
Dirk Zimmermann <dz@pep.security>
2021-09-09 10:35:55 +0200

    IPS-9 Move test

--- xcode/PEPSCTPTransport/PEPSCTPTransportTests/PEPSCTPTransportTests.m
+++ xcode/PEPSCTPTransport/PEPSCTPTransportTests/PEPSCTPTransportTests.m
@@ -7,10 +7,31 @@
 
 #import <XCTest/XCTest.h>
 
+#import "PEPSCTPTransport.h"
+#import "TestConstants.h"
+
 @interface PEPSCTPTransportTests : XCTestCase
 
 @end
 
 @implementation PEPSCTPTransportTests
+
+- (void)testStartupWithoutConfigLeadsToError
+{
+    NSError *error = nil;
+    PEPSCTPTransport *transport = [[PEPSCTPTransport alloc]
+                                   initWithSignalStatusChangeDelegate:nil
+                                   signalSendToResultDelegate:nil
+                                   signalIncomingMessageDelegate:nil
+                                   callbackExecutionType:PEPTransportCallbackExcecutionTypePolling
+                                   error:&error];
+
+    XCTAssertNil(error);
+
+    PEPTransportStatusCode statusCode = 0;
+    [transport startup:NULL transportStatusCode:&statusCode error:&error];
+    XCTAssertNotNil(error);
+    XCTAssertEqual(statusCode, PEPTransportStatusCodeConnectionNoConfig);
+}
 
 @end
--- xcode/PEPSCTPTransport/PEPSCTPTransportTests/Util/PEPMessageASN1Tests.m
+++ xcode/PEPSCTPTransport/PEPSCTPTransportTests/Util/PEPMessageASN1Tests.m
@@ -7,31 +7,10 @@
 
 #import <XCTest/XCTest.h>
 
-#import "PEPSCTPTransport.h"
-#import "TestConstants.h"
-
 @interface PEPMessageASN1Tests : XCTestCase
 
 @end
 
 @implementation PEPMessageASN1Tests
-
-- (void)testStartupWithoutConfigLeadsToError
-{
-    NSError *error = nil;
-    PEPSCTPTransport *transport = [[PEPSCTPTransport alloc]
-                                   initWithSignalStatusChangeDelegate:nil
-                                   signalSendToResultDelegate:nil
-                                   signalIncomingMessageDelegate:nil
-                                   callbackExecutionType:PEPTransportCallbackExcecutionTypePolling
-                                   error:&error];
-
-    XCTAssertNil(error);
-
-    PEPTransportStatusCode statusCode = 0;
-    [transport startup:NULL transportStatusCode:&statusCode error:&error];
-    XCTAssertNotNil(error);
-    XCTAssertEqual(statusCode, PEPTransportStatusCodeConnectionNoConfig);
-}
 
 @end
