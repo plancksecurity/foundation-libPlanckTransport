a2a04a6f7116305fbc27aa609e00e0555a7843a5
heck <heck@pep.foundation>
2021-09-25 17:36:27 +0200

    transport.hh: still have to use "0x00ffffff" for the initial TSC

--- src/transport.hh
+++ src/transport.hh
@@ -33,7 +33,7 @@ namespace pEp {
 
         // Ctors
         // -----
-        explicit Transport(PEP_transport_id id) : id(id), current_status(PEP_tsc_shut_down) {}
+        explicit Transport(PEP_transport_id id) : id(id), current_status(0x00ffffff) {}
         Transport(const Transport&) = delete;
         Transport& operator=(const Transport&) = delete;
         virtual ~Transport() = default;
