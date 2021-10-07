c267dd808a5406cac7e11446da9b713bc7f835b7
Volker Birk <vb@pep-project.org>
2021-10-07 19:11:15 +0200

    we do not need constructors in an interface

--- src/transport.hh
+++ src/transport.hh
@@ -34,15 +34,6 @@ namespace pEp {
             virtual ~Config() = 0;
         };
 
-        // Ctors
-        // -----
-        explicit Transport(PEP_transport_id id) { }
-        Transport(const Transport&) = delete;
-        Transport(const Transport&&) = delete;
-        Transport& operator=(const Transport&) = delete;
-        Transport& operator=(const Transport&&) = delete;
-        virtual ~Transport() = default;
-
         // required for compat with the c interface transport.h because of missing polymorphism in c
         virtual PEP_transport_id get_id() = 0;
 
