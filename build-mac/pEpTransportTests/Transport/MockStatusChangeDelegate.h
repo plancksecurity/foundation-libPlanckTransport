c033422b0f5f674bc73200be29e9255058fe09ec
Dirk Zimmermann <dz@pep.security>
2022-01-26 13:44:25 +0100

    IPS-157 Start to mock the status change delegate

---
+++ build-mac/pEpTransportTests/Transport/MockStatusChangeDelegate.h
@@ -0,0 +1,16 @@
+//
+//  MockStatusChangeDelegate.h
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 26.01.22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MockStatusChangeDelegate : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ build-mac/pEpTransportTests/Transport/MockStatusChangeDelegate.m
@@ -0,0 +1,12 @@
+//
+//  MockStatusChangeDelegate.m
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 26.01.22.
+//
+
+#import "MockStatusChangeDelegate.h"
+
+@implementation MockStatusChangeDelegate
+
+@end
