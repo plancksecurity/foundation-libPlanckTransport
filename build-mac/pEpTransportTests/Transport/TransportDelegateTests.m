6fae889532f12528b5b07926ab8edaabb630d758
Dirk Zimmermann <dz@pep.security>
2022-01-26 14:50:39 +0100

    IPS-157 Put some startup into setUp

--- build-mac/pEpTransportTests/Transport/TransportDelegateTests.m
+++ build-mac/pEpTransportTests/Transport/TransportDelegateTests.m
@@ -12,6 +12,9 @@
 
 @interface TransportDelegateTests : XCTestCase
 
+@property (nonatomic) MockStatusChangeDelegate *statusChangeDelegate;
+@property (nonatomic) TransportMock *transport;
+
 @end
 
 @implementation TransportDelegateTests
@@ -19,6 +22,26 @@
 #pragma mark - Setup, Teardown
 
 - (void)setUp {
+    self.statusChangeDelegate = [MockStatusChangeDelegate new];
+    NSError *error = nil;
+    self.transport = [[TransportMock alloc]
+                      initWithSignalStatusChangeDelegate:self.statusChangeDelegate
+                      signalSendToResultDelegate:nil
+                      signalIncomingMessageDelegate:nil
+                      error:&error];
+    XCTAssertNotNil(self.transport);
+    XCTAssertNil(error);
+
+    error = nil;
+    self.transport.startupShouldSucceed = YES;
+    PEPTransportStatusCode statusCode;
+    BOOL success = [self.transport startupWithTransportStatusCode:&statusCode error:&error];
+    XCTAssertTrue(success);
+    XCTAssertNil(error);
+    XCTAssertEqual(self.statusChangeDelegate.statusChanges.count, 1);
+    NSNumber *num = [self.statusChangeDelegate.statusChanges firstObject];
+    XCTAssertNotNil(num);
+    XCTAssertEqual(num.integerValue, PEPTransportStatusCodeConnectionUp);
 }
 
 - (void)tearDown {
@@ -27,29 +50,13 @@
 #pragma mark - Tests
 
 - (void)testStartupSuccess {
-    MockStatusChangeDelegate *statusChangeDelegate = [MockStatusChangeDelegate new];
-    NSError *error = nil;
-    TransportMock *transport = [[TransportMock alloc]
-                                initWithSignalStatusChangeDelegate:statusChangeDelegate
-                                signalSendToResultDelegate:nil
-                                signalIncomingMessageDelegate:nil
-                                error:&error];
-    XCTAssertNotNil(transport);
-    XCTAssertNil(error);
-
-    error = nil;
-    transport.startupShouldSucceed = YES;
-    PEPTransportStatusCode statusCode;
-    BOOL success = [transport startupWithTransportStatusCode:&statusCode error:&error];
-    XCTAssertTrue(success);
-    XCTAssertNil(error);
-    XCTAssertEqual(statusChangeDelegate.statusChanges.count, 1);
-    NSNumber *num = [statusChangeDelegate.statusChanges firstObject];
-    XCTAssertNotNil(num);
-    XCTAssertEqual(num.integerValue, PEPTransportStatusCodeConnectionUp);
+    // Basically handled by setUp.
 }
 
 - (void)testStartupDirectError {
+    // Use local variables, note that there was already a successful startup in setUp,
+    // that we simply ignore.
+
     MockStatusChangeDelegate *statusChangeDelegate = [MockStatusChangeDelegate new];
     NSError *error = nil;
     TransportMock *transport = [[TransportMock alloc]
