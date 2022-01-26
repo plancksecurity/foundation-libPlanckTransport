234f9842e6b0a964264edda59d12d6112d796950
Dirk Zimmermann <dz@pep.security>
2022-01-26 15:09:48 +0100

    IPS-157 MockIncomingMessageDelegate

---
+++ build-mac/pEpTransportTests/Transport/MockIncomingMessageDelegate.h
@@ -0,0 +1,16 @@
+//
+//  MockIncomingMessageDelegate.h
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 26.01.22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MockIncomingMessageDelegate : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ build-mac/pEpTransportTests/Transport/MockIncomingMessageDelegate.m
@@ -0,0 +1,12 @@
+//
+//  MockIncomingMessageDelegate.m
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 26.01.22.
+//
+
+#import "MockIncomingMessageDelegate.h"
+
+@implementation MockIncomingMessageDelegate
+
+@end
