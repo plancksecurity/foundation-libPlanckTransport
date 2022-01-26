c7759c72f671ffb77bcac831cbef4ae31bbea565
Dirk Zimmermann <dz@pep.security>
2022-01-26 11:59:18 +0100

    IPS-157 Init first

--- build-mac/pEpTransportTests/Transport/TransportMock.m
+++ build-mac/pEpTransportTests/Transport/TransportMock.m
@@ -15,17 +15,17 @@
 
 @synthesize signalStatusChangeDelegate;
 
-- (BOOL)configureWithConfig:(PEPTransportConfig * _Nullable)config
-        transportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode
-                      error:(NSError * _Nullable __autoreleasing * _Nullable)error {
-    return NO;
-}
-
 - (instancetype _Nullable)initWithSignalStatusChangeDelegate:(id<PEPTransportStatusChangeDelegate> _Nullable)signalStatusChangeDelegate
                                   signalSendToResultDelegate:(id<PEPTransportSendToResultDelegate> _Nullable)signalSendToResultDelegate
                                signalIncomingMessageDelegate:(id<PEPTransportIncomingMessageDelegate> _Nullable)signalIncomingMessageDelegate
                                                        error:(NSError * _Nullable __autoreleasing * _Nullable)error {
     return nil;
+}
+
+- (BOOL)configureWithConfig:(PEPTransportConfig * _Nullable)config
+        transportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode
+                      error:(NSError * _Nullable __autoreleasing * _Nullable)error {
+    return NO;
 }
 
 - (PEPMessage * _Nullable)nextMessageWithPEPSession:(PEPSession * _Nullable)pEpsession
