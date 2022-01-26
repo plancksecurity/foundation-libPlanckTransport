c0f336e46c1a8f9749cf7efac03597c655b0525d
Dirk Zimmermann <dz@pep.security>
2022-01-26 11:51:29 +0100

    IPS-157 Start the mock for PEPTransportProtocol

---
+++ build-mac/pEpTransportTests/Transport/TransportMock.h
@@ -0,0 +1,17 @@
+//
+//  TransportMock.h
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 26.01.22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// A mock of `PEPTransportProtocol`.
+@interface TransportMock : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ build-mac/pEpTransportTests/Transport/TransportMock.m
@@ -0,0 +1,12 @@
+//
+//  TransportMock.m
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 26.01.22.
+//
+
+#import "TransportMock.h"
+
+@implementation TransportMock
+
+@end
