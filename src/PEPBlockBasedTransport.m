06523a8f6290dbb12d27774f99f40dabcec195f3
Dirk Zimmermann <dz@pep.security>
2022-01-26 10:26:30 +0100

    IPS-157 Add empty PEPBlockBasedTransport and import the protocol

---
+++ src/PEPBlockBasedTransport.m
@@ -0,0 +1,12 @@
+//
+//  PEPBlockBasedTransport.m
+//  pEpTransport_macOS
+//
+//  Created by Dirk Zimmermann on 26.01.22.
+//
+
+#import "PEPBlockBasedTransport.h"
+
+@implementation PEPBlockBasedTransport
+
+@end
