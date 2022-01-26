db6560a002011207dc2f886134b965783162a3f9
Dirk Zimmermann <dz@pep.security>
2022-01-26 11:54:59 +0100

    IPS-157 Format

--- build-mac/pEpTransportTests/Transport/TransportMock.m
+++ build-mac/pEpTransportTests/Transport/TransportMock.m
@@ -15,27 +15,39 @@
 
 @synthesize signalStatusChangeDelegate;
 
-- (BOOL)configureWithConfig:(PEPTransportConfig * _Nullable)config transportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode error:(NSError * _Nullable __autoreleasing * _Nullable)error {
+- (BOOL)configureWithConfig:(PEPTransportConfig * _Nullable)config
+        transportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode
+                      error:(NSError * _Nullable __autoreleasing * _Nullable)error {
     return NO;
 }
 
-- (instancetype _Nullable)initWithSignalStatusChangeDelegate:(id<PEPTransportStatusChangeDelegate> _Nullable)signalStatusChangeDelegate signalSendToResultDelegate:(id<PEPTransportSendToResultDelegate> _Nullable)signalSendToResultDelegate signalIncomingMessageDelegate:(id<PEPTransportIncomingMessageDelegate> _Nullable)signalIncomingMessageDelegate error:(NSError * _Nullable __autoreleasing * _Nullable)error {
+- (instancetype _Nullable)initWithSignalStatusChangeDelegate:(id<PEPTransportStatusChangeDelegate> _Nullable)signalStatusChangeDelegate
+                                  signalSendToResultDelegate:(id<PEPTransportSendToResultDelegate> _Nullable)signalSendToResultDelegate
+                               signalIncomingMessageDelegate:(id<PEPTransportIncomingMessageDelegate> _Nullable)signalIncomingMessageDelegate
+                                                       error:(NSError * _Nullable __autoreleasing * _Nullable)error {
     return nil;
 }
 
-- (PEPMessage * _Nullable)nextMessageWithPEPSession:(PEPSession * _Nullable)pEpsession transportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode error:(NSError * _Nullable __autoreleasing * _Nullable)error {
+- (PEPMessage * _Nullable)nextMessageWithPEPSession:(PEPSession * _Nullable)pEpsession
+                                transportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode
+                                              error:(NSError * _Nullable __autoreleasing * _Nullable)error {
     return nil;
 }
 
-- (BOOL)sendMessage:(nonnull PEPMessage *)msg pEpSession:(PEPSession * _Nullable)pEpSession transportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode error:(NSError * _Nullable __autoreleasing * _Nullable)error {
+- (BOOL)sendMessage:(nonnull PEPMessage *)msg
+         pEpSession:(PEPSession * _Nullable)pEpSession
+transportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode
+              error:(NSError * _Nullable __autoreleasing * _Nullable)error {
     return NO;
 }
 
-- (BOOL)shutdownWithTransportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode error:(NSError * _Nullable __autoreleasing * _Nullable)error {
+- (BOOL)shutdownWithTransportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode
+                                  error:(NSError * _Nullable __autoreleasing * _Nullable)error {
     return NO;
 }
 
-- (BOOL)startupWithTransportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode error:(NSError * _Nullable __autoreleasing * _Nullable)error {
+- (BOOL)startupWithTransportStatusCode:(out PEPTransportStatusCode * _Nonnull)transportStatusCode
+                                 error:(NSError * _Nullable __autoreleasing * _Nullable)error {
     return NO;
 }
 
