b2b9e8cf0951082a26b02822dc7f0685ed27bafe
Dirk Zimmermann <dz@pep.security>
2022-01-26 14:43:48 +0100

    IPS-157 Test for successful shutdown

--- build-mac/pEpTransportTests/Transport/TransportDelegateTests.m
+++ build-mac/pEpTransportTests/Transport/TransportDelegateTests.m
@@ -68,4 +68,34 @@
     XCTAssertNil(num);
 }
 
+- (void)testShutdownSuccess {
+    MockStatusChangeDelegate *statusChangeDelegate = [MockStatusChangeDelegate new];
+    NSError *error = nil;
+    TransportMock *transport = [[TransportMock alloc]
+                                initWithSignalStatusChangeDelegate:statusChangeDelegate
+                                signalSendToResultDelegate:nil
+                                signalIncomingMessageDelegate:nil
+                                error:&error];
+    XCTAssertNotNil(transport);
+    XCTAssertNil(error);
+
+    error = nil;
+    transport.startupShouldSucceed = YES;
+    PEPTransportStatusCode statusCode;
+    BOOL success = [transport startupWithTransportStatusCode:&statusCode error:&error];
+    XCTAssertTrue(success);
+    XCTAssertNil(error);
+    XCTAssertEqual(statusChangeDelegate.statusChanges.count, 1);
+    NSNumber *num = [statusChangeDelegate.statusChanges firstObject];
+    XCTAssertNotNil(num);
+    XCTAssertEqual(num.integerValue, PEPTransportStatusCodeConnectionUp);
+
+    error = nil;
+    BOOL success2 = [transport shutdownWithTransportStatusCode:&statusCode error:&error];
+    XCTAssertTrue(success2);
+    NSArray *expectedStatus = @[[NSNumber numberWithInteger:PEPTransportStatusCodeConnectionUp],
+                                [NSNumber numberWithInteger:PEPTransportStatusCodeConnectionDown]];
+    XCTAssertEqualObjects(statusChangeDelegate.statusChanges, expectedStatus);
+}
+
 @end
