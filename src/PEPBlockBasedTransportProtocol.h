7028b825266cb02a3c52d83f1b8e2609e41555e7
Dirk Zimmermann <dz@pep.security>
2022-01-26 10:21:48 +0100

    IPS-157 Add (empty) PEPBlockBasedTransportProtocol.h

---
+++ src/PEPBlockBasedTransportProtocol.h
@@ -0,0 +1,16 @@
+//
+//  PEPBlockBasedTransportProtocol.h
+//  pEpTransport_macOS
+//
+//  Created by Dirk Zimmermann on 26.01.22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol PEPBlockBasedTransportProtocol <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
