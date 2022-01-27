871dfc427998d9ac5987f4d9c22bddc46b7b47da
Dirk Zimmermann <dz@pep.security>
2022-01-27 15:07:09 +0100

    IPS-157 Start to implement PEPBlockBasedTransportProtocol

--- src/PEPBlockBasedTransport.m
+++ src/PEPBlockBasedTransport.m
@@ -9,4 +9,31 @@
 
 @implementation PEPBlockBasedTransport
 
+- (instancetype _Nullable)initWithTransport:(nonnull id<PEPTransportProtocol>)transport
+                          transportDelegate:(nonnull id<PEPBlockBasedTransportDelegate>)transportDelegate {
+    return nil;
+}
+
+- (BOOL)configureWithConfig:(PEPTransportConfig * _Nullable)config
+        transportStatusCode:(out nonnull PEPTransportStatusCode *)transportStatusCode
+                      error:(NSError * _Nullable __autoreleasing * _Nullable)error {
+    return NO;
+}
+
+- (void)sendMessage:(nonnull PEPMessage *)msg
+     withPEPSession:(PEPSession * _Nullable)pEpSession
+          onSuccess:(nonnull void (^)(PEPTransportStatusCode))successCallback
+            onError:(nonnull void (^)(PEPTransportStatusCode, NSError * _Nonnull))errorCallback {
+}
+
+- (void)shutdownOnSuccess:(nonnull void (^)(PEPTransportStatusCode))successCallback
+                  onError:(nonnull void (^)(PEPTransportStatusCode,
+                                            NSError * _Nonnull))errorCallback {
+}
+
+- (void)startupWithOnSuccess:(nonnull void (^)(PEPTransportStatusCode))successCallback
+                     onError:(nonnull void (^)(PEPTransportStatusCode,
+                                               NSError * _Nonnull))errorCallback {
+}
+
 @end
