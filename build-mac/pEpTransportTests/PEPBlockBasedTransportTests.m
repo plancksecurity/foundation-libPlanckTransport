81141d822b2a176ebaf9e85f62ada8c3b342cee3
Dirk Zimmermann <dz@pep.security>
2022-01-28 10:33:34 +0100

    IPS-157 Init the normal transport (mock)

--- build-mac/pEpTransportTests/PEPBlockBasedTransportTests.m
+++ build-mac/pEpTransportTests/PEPBlockBasedTransportTests.m
@@ -7,7 +7,13 @@
 
 #import <XCTest/XCTest.h>
 
+#import "PEPBlockBasedTransport.h"
+#import "TransportMock.h"
+
 @interface PEPBlockBasedTransportTests : XCTestCase
+
+@property (nonatomic) TransportMock *transport;
+@property (nonatomic) PEPBlockBasedTransport *blockTransport;
 
 @end
 
@@ -16,6 +22,14 @@
 #pragma mark - Setup, Teardown
 
 - (void)setUp {
+    NSError *error = nil;
+    self.transport = [[TransportMock alloc]
+                      initWithSignalStatusChangeDelegate:nil
+                      signalSendToResultDelegate:nil
+                      signalIncomingMessageDelegate:nil
+                      error:&error];
+    XCTAssertNotNil(self.transport);
+    XCTAssertNil(error);
 }
 
 - (void)tearDown {
