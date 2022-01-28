478b47f53252a2334630043d2ffe90f2cab979d1
Dirk Zimmermann <dz@pep.security>
2022-01-28 10:09:52 +0100

    IPS-157 Divide the future code

--- build-mac/pEpTransportTests/PEPBlockBasedTransportTests.m
+++ build-mac/pEpTransportTests/PEPBlockBasedTransportTests.m
@@ -13,24 +13,18 @@
 
 @implementation PEPBlockBasedTransportTests
 
+#pragma mark - Setup, Teardown
+
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
+#pragma mark - Tests
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testSuccessfulStartupAndShutdown {
+    // Basically handled by setUp and tearDown.
 }
 
 @end
