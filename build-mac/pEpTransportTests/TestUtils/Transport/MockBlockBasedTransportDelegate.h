f4c6571c3445e9d9292625b764fdff7b0d5f0fb9
Dirk Zimmermann <dz@pep.security>
2022-01-28 10:08:06 +0100

    IPS-157 Flesh out the block delegate mock

--- build-mac/pEpTransportTests/TestUtils/Transport/MockBlockBasedTransportDelegate.h
+++ build-mac/pEpTransportTests/TestUtils/Transport/MockBlockBasedTransportDelegate.h
@@ -6,12 +6,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
 
 #import "PEPBlockBasedTransport.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MockBlockBasedTransportDelegate : NSObject <PEPBlockBasedTransportDelegate>
+
+@property (nonatomic, nullable) XCTestExpectation *expConnectionDown;
+@property (nonatomic, nullable) XCTestExpectation *expIncomingMessage;
+
+@property (nonatomic, readonly) PEPMessage *lastMessageReceived;
+@property (nonatomic) PEPTransportStatusCode lastMessageReceivedStatusCode;
+@property (nonatomic) PEPTransportStatusCode lastConnectionDownStatusCode;
 
 @end
 
--- build-mac/pEpTransportTests/TestUtils/Transport/MockBlockBasedTransportDelegate.m
+++ build-mac/pEpTransportTests/TestUtils/Transport/MockBlockBasedTransportDelegate.m
@@ -7,15 +7,26 @@
 
 #import "MockBlockBasedTransportDelegate.h"
 
+@interface MockBlockBasedTransportDelegate ()
+
+@property (nonatomic) PEPMessage *lastMessageReceived;
+
+@end
+
 @implementation MockBlockBasedTransportDelegate
 
 - (void)connectionStoppedWithtransportID:(PEPTransportID)transportID
                               statusCode:(PEPTransportStatusCode)statusCode {
+    self.lastConnectionDownStatusCode = statusCode;
+    [self.expConnectionDown fulfill];
 }
 
 - (void)signalIncomingMessage:(nonnull PEPMessage *)message
                   transportID:(PEPTransportID)transportID
                    statusCode:(PEPTransportStatusCode)statusCode {
+    self.lastMessageReceived = message;
+    self.lastMessageReceivedStatusCode = statusCode;
+    [self.expIncomingMessage fulfill];
 }
 
 @end
