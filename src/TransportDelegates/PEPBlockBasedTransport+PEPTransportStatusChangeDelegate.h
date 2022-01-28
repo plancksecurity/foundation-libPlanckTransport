5c627d4dc537bdee1d9f704c9b5b7ac5cc0f6229
Dirk Zimmermann <dz@pep.security>
2022-01-28 11:05:36 +0100

    IPS-157 Start to implement one delegate

---
+++ src/TransportDelegates/PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.h
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 28.01.22.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPBlockBasedTransport.h"
+#import "PEPTransportProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPBlockBasedTransport (PEPTransportStatusChangeDelegate)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/TransportDelegates/PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.m
@@ -0,0 +1,12 @@
+//
+//  PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.m
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 28.01.22.
+//
+
+#import "PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.h"
+
+@implementation PEPBlockBasedTransport (PEPTransportStatusChangeDelegate)
+
+@end
