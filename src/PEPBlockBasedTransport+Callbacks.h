6f49a0c8808a9ca18c65d78ecbbdea2eda1f5be4
Dirk Zimmermann <dz@pep.security>
2022-01-31 11:48:04 +0100

    IPS-157 Move the interface decl to the right place

--- src/PEPBlockBasedTransport+Callbacks.h
+++ src/PEPBlockBasedTransport+Callbacks.h
@@ -14,6 +14,10 @@ NS_ASSUME_NONNULL_BEGIN
 /// that need to call back into the mothersip.
 @interface PEPBlockBasedTransport (Callbacks)
 
+/// Tries to find pending startup callbacks and informs them of the given status code, marking it as a success.
+/// @return `YES` if there was a least one callback invoked, `NO` if there was none.
+- (BOOL)invokePendingStartSuccessCallbacksWithStatusCode:(PEPTransportStatusCode)statusCode;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- src/TransportDelegates/PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.h
+++ src/TransportDelegates/PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.h
@@ -14,10 +14,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PEPBlockBasedTransport (PEPTransportStatusChangeDelegate) <PEPTransportStatusChangeDelegate>
 
-/// Tries to find pending startup callbacks and informs them of the given status code, marking it as a success.
-/// @return `YES` if there was a least one callback invoked, `NO` if there was none.
-- (BOOL)invokePendingStartSuccessCallbacksWithStatusCode:(PEPTransportStatusCode)statusCode;
-
 @end
 
 NS_ASSUME_NONNULL_END
