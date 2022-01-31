4b3eef972e87e4171d220a2f27a20ea430065c7c
Dirk Zimmermann <dz@pep.security>
2022-01-31 14:59:29 +0100

    IPS-157 Comment

--- src/TransportDelegates/PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.m
+++ src/TransportDelegates/PEPBlockBasedTransport+PEPTransportStatusChangeDelegate.m
@@ -13,6 +13,11 @@
 
 - (void)signalStatusChangeWithTransportID:(PEPTransportID)transportID
                                statusCode:(PEPTransportStatusCode)statusCode {
+    // The general idea here is, that a transport is either in "startup mode"
+    // (that is, the user is waiting for the startup to succeed or fail),
+    // or it is in "shutdown mode" (like "startup mode", but when shutting down,
+    // or "in the middle".
+    // That's the basis for figuring out which stored callbacks to call, if any.
     BOOL invoked = NO;
     invoked = [self invokePendingStartCallbacksWithStatusCode:statusCode];
 
