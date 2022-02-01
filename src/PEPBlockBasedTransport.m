4cc8e99c141cdf2c887659882c637b157935a423
Dirk Zimmermann <dz@pep.security>
2022-02-01 13:18:29 +0100

    IPS-157 Treat concurrent start, stop as error

--- src/PEPBlockBasedTransport.m
+++ src/PEPBlockBasedTransport.m
@@ -55,7 +55,20 @@ static NSString * const s_ErrorDomain = @"PEPBlockBasedTransport";
 - (void)startupWithOnSuccess:(nonnull void (^)(PEPTransportStatusCode))successCallback
                      onError:(nonnull void (^)(PEPTransportStatusCode,
                                                NSError * _Nonnull))errorCallback {
+    // Treat concurrent startups from different threads as a developer error
     NSAssert(self.startupCallback == nil, @"startup invoked with callback already set");
+
+    // Report concurrent startups from different threads as error
+    if (self.startupCallback == nil) {
+        // TODO: Find a better status code
+        PEPTransportStatusCode invalidStateStatusCode = PEPTransportStatusCodeConfigIncompleteOrWrong;
+        NSError *error = [NSError errorWithDomain:s_ErrorDomain
+                                             code:invalidStateStatusCode
+                                         userInfo:nil];
+        errorCallback(invalidStateStatusCode, error);
+
+        return;
+    }
 
     PEPTransportStatusCallbacks *callback = [PEPTransportStatusCallbacks
                                              callbacksWithSuccessCallback:successCallback
@@ -86,7 +99,20 @@ static NSString * const s_ErrorDomain = @"PEPBlockBasedTransport";
 - (void)shutdownOnSuccess:(nonnull void (^)(PEPTransportStatusCode))successCallback
                   onError:(nonnull void (^)(PEPTransportStatusCode,
                                             NSError * _Nonnull))errorCallback {
+    // Treat concurrent shutdowns from different threads as a developer error
     NSAssert(self.shutdownCallback == nil, @"shutdown invoked with callback already set");
+
+    // Report concurrent shutdowns from different threads as error
+    if (self.startupCallback == nil) {
+        // TODO: Find a better status code
+        PEPTransportStatusCode invalidStateStatusCode = PEPTransportStatusCodeConfigIncompleteOrWrong;
+        NSError *error = [NSError errorWithDomain:s_ErrorDomain
+                                             code:invalidStateStatusCode
+                                         userInfo:nil];
+        errorCallback(invalidStateStatusCode, error);
+
+        return;
+    }
 
     PEPTransportStatusCallbacks *callback = [PEPTransportStatusCallbacks
                                              callbacksWithSuccessCallback:successCallback
