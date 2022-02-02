91a374966d8b40a1d9330d1f89503ff44004b0a2
Dirk Zimmermann <dz@pep.security>
2022-02-02 12:19:26 +0100

    IPS-157 Comment

--- src/PEPBlockBasedTransport+ForDelegates.h
+++ src/PEPBlockBasedTransport+ForDelegates.h
@@ -12,6 +12,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// The interface that is used by the various delegate implementations (in class extensions),
 /// that need to call back into the mothersip, that is to say, the internal interface for delegates.
+///
+/// @note These are methods that must be defined in the main compilation unit of PEPBlockBasedTransport
+/// since they use internal instance variables. Otherwise a pure class-extension could have been used.
 @interface PEPBlockBasedTransport (ForDelegates)
 
 /// Tries to find pending startup callbacks and informs them of the given status code, marking it as a success.
