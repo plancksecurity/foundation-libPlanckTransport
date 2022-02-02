740ebe2a868848d7f2b21acbe90ba1344796fedd
Dirk Zimmermann <dz@pep.security>
2022-02-02 11:32:25 +0100

    IPS-157 Add empty send result delegate

---
+++ src/TransportDelegates/PEPBlockBasedTransport+PEPTransportSendToResultDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PEPBlockBasedTransport+PEPTransportSendToResultDelegate.h
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 02.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPBlockBasedTransport.h"
+#import "PEPTransportProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPBlockBasedTransport (PEPTransportSendToResultDelegate) <PEPTransportSendToResultDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/TransportDelegates/PEPBlockBasedTransport+PEPTransportSendToResultDelegate.m
@@ -0,0 +1,12 @@
+//
+//  PEPBlockBasedTransport+PEPTransportSendToResultDelegate.m
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 02.02.22.
+//
+
+#import "PEPBlockBasedTransport+PEPTransportSendToResultDelegate.h"
+
+@implementation PEPBlockBasedTransport (PEPTransportSendToResultDelegate)
+
+@end
