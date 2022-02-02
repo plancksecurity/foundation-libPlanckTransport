7635f45ef55ca9ed9b0b2b54d1b3941d5cd69df9
Dirk Zimmermann <dz@pep.security>
2022-02-02 10:12:39 +0100

    IPS-157 Extension for error handling

---
+++ src/PEPBlockBasedTransport+Error.h
@@ -0,0 +1,18 @@
+//
+//  PEPBlockBasedTransport+Error.h
+//  pEpTransport_macOS
+//
+//  Created by Dirk Zimmermann on 02.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPBlockBasedTransport.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPBlockBasedTransport (Error)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/PEPBlockBasedTransport+Error.m
@@ -0,0 +1,12 @@
+//
+//  PEPBlockBasedTransport+Error.m
+//  pEpTransport_macOS
+//
+//  Created by Dirk Zimmermann on 02.02.22.
+//
+
+#import "PEPBlockBasedTransport+Error.h"
+
+@implementation PEPBlockBasedTransport (Error)
+
+@end
