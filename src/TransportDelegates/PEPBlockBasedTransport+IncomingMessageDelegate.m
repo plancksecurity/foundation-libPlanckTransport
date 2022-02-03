041585f5b1aefeec0b6919a3f75c40ae40983576
Dirk Zimmermann <dz@pep.security>
2022-02-03 09:15:42 +0100

    IPS-157 Incoming message delegate

---
+++ src/TransportDelegates/PEPBlockBasedTransport+IncomingMessageDelegate.m
@@ -0,0 +1,17 @@
+//
+//  PEPBlockBasedTransport+IncomingMessageDelegate.m
+//  pEpTransport_macOS
+//
+//  Created by Dirk Zimmermann on 03.02.22.
+//
+
+#import "PEPBlockBasedTransport+IncomingMessageDelegate.h"
+
+@implementation PEPBlockBasedTransport (IncomingMessageDelegate)
+
+- (void)signalIncomingMessageWithTransportID:(PEPTransportID)transportID
+                                  statusCode:(PEPTransportStatusCode)statusCode {
+    // TODO
+}
+
+@end
