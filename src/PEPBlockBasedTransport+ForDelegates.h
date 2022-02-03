4d008b5a2f611fae0f9b9c9f332b4ef3dd1cdd88
Dirk Zimmermann <dz@pep.security>
2022-02-03 09:55:53 +0100

    IPS-157 Comment

--- src/PEPBlockBasedTransport+ForDelegates.h
+++ src/PEPBlockBasedTransport+ForDelegates.h
@@ -13,13 +13,14 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /// The interface that is used by the various delegate implementations (in class extensions),
-/// that need to call back into the mothersip, that is to say, the internal interface for delegates.
-///
-/// @note These are methods that must be defined in the main compilation unit of PEPBlockBasedTransport
-/// since they use internal instance variables. Otherwise a pure class-extension could have been used.
+/// that need to use, e.g., instance variables of the class, that by nature can only be defined
+/// in the class itself.
 @interface PEPBlockBasedTransport (ForDelegates)
 
+/// Shadows the definition in the class extension, in order to give the delegate acces to this instance variable.
 @property (nonatomic, nullable) PEPTransportStatusCallbacks *startupCallback;
+
+/// Shadows the definition in the class extension, in order to give the delegate acces to this instance variable.
 @property (nonatomic, nullable) PEPTransportStatusCallbacks *shutdownCallback;
 
 /// Finds the callbacks for a given message ID and removes them.
