c98e213c7f3e82dc56d729b694fce7036ac5067c
Dirk Zimmermann <dz@pep.security>
2022-02-03 13:33:17 +0100

    IPS-157 Indicate whether any callbacks have been informed

--- src/TransportDelegates/PEPBlockBasedTransport+StatusChangeDelegate.m
+++ src/TransportDelegates/PEPBlockBasedTransport+StatusChangeDelegate.m
@@ -15,7 +15,8 @@
 
 @implementation PEPBlockBasedTransport (PEPTransportStatusChangeDelegateHelpers)
 
-- (void)signalErrorWithStatusCode:(PEPTransportStatusCode)statusCode
+/// @return `YES` if at least one callback was informed, `NO` otherwise.
+- (BOOL)signalErrorWithStatusCode:(PEPTransportStatusCode)statusCode
                       toCallbacks:(NSMutableSet<PEPTransportStatusCallbacks *> *)callbackSet {
     NSError *error = [self errorWithTransportStatusCode:statusCode];
     NSSet *localCallbacks = nil;
@@ -36,9 +37,12 @@
             [callbackSet removeObject:callbacks];
         }
     }
+
+    return localCallbacks.count > 0;
 }
 
-- (void)signalSuccessWithStatusCode:(PEPTransportStatusCode)statusCode
+/// @return `YES` if at least one callback was informed, `NO` otherwise.
+- (BOOL)signalSuccessWithStatusCode:(PEPTransportStatusCode)statusCode
                         toCallbacks:(NSMutableSet<PEPTransportStatusCallbacks *> *)callbackSet {
     NSSet *localCallbacks = nil;
 
@@ -58,6 +62,8 @@
             [callbackSet removeObject:callbacks];
         }
     }
+
+    return localCallbacks.count > 0;
 }
 
 @end
@@ -68,22 +74,29 @@
 
 - (void)signalStatusChangeWithTransportID:(PEPTransportID)transportID
                                statusCode:(PEPTransportStatusCode)statusCode {
+    BOOL delivered1 = NO;
+    BOOL delivered2 = NO;
+
     if (statusCode == PEPTransportStatusCodeConnectionDown) {
         // Connection down is an error for startup, but success for shutdown.
-        [self signalErrorWithStatusCode:statusCode toCallbacks:self.startupCallbacks];
-        [self signalSuccessWithStatusCode:statusCode toCallbacks:self.shutdownCallbacks];
+        delivered1 = [self signalErrorWithStatusCode:statusCode toCallbacks:self.startupCallbacks];
+        delivered2 = [self signalSuccessWithStatusCode:statusCode toCallbacks:self.shutdownCallbacks];
     } else if (statusCode == PEPTransportStatusCodeConnectionUp) {
         // Connection up is success for any startup, but an error for shutdown.
-        [self signalSuccessWithStatusCode:statusCode toCallbacks:self.startupCallbacks];
-        [self signalErrorWithStatusCode:statusCode toCallbacks:self.shutdownCallbacks];
+        delivered1 = [self signalSuccessWithStatusCode:statusCode toCallbacks:self.startupCallbacks];
+        delivered2 = [self signalErrorWithStatusCode:statusCode toCallbacks:self.shutdownCallbacks];
     } else if ([PEPTransportStatusCodeUtil isErrorStatusCode:statusCode]) {
         // Consider other errors as errors for either type of pending callbacks.
-        [self signalErrorWithStatusCode:statusCode toCallbacks:self.startupCallbacks];
-        [self signalErrorWithStatusCode:statusCode toCallbacks:self.shutdownCallbacks];
+        delivered1 = [self signalErrorWithStatusCode:statusCode toCallbacks:self.startupCallbacks];
+        delivered2 = [self signalErrorWithStatusCode:statusCode toCallbacks:self.shutdownCallbacks];
     } else {
         // Any non-error status code would land here. Nothing to do, since:
         //  * startup only cares for connection up, or errors
         //  * shutdown only cares for connection down, or errors
+    }
+
+    if (!delivered1 && !delivered2) {
+        // We haven't delivered that status code to anyone.
     }
 }
 
