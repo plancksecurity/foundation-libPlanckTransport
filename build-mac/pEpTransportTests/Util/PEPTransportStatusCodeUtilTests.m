529b43364710bda64c08bea077e9b000c9a4b396
Dirk Zimmermann <dz@pep.security>
2022-02-10 13:03:07 +0100

    IPS-157 Start PEPTransportStatusCodeUtilTests

---
+++ build-mac/pEpTransportTests/Util/PEPTransportStatusCodeUtilTests.m
@@ -0,0 +1,21 @@
+//
+//  PEPTransportStatusCodeUtilTests.m
+//  pEpTransportTests
+//
+//  Created by Dirk Zimmermann on 10.02.22.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "PEPTransportStatusCodeUtil.h"
+
+@interface PEPTransportStatusCodeUtilTests : XCTestCase
+
+@end
+
+@implementation PEPTransportStatusCodeUtilTests
+
+- (void)testSeverity {
+}
+
+@end
