2383a68afdeaefa82b3d647e9aae0a287c707be8
Dirk Zimmermann <dz@planck.security>
2023-05-26 11:33:38 +0200

    CORE-15 "Take the header files" to the right location

--- build-android/jni/Android.mk
+++ build-android/jni/Android.mk
@@ -11,12 +11,12 @@ LOCAL_MODULE    := pEpTransport
 LOCAL_CPPFLAGS += -std=c99
 
 #FIXME: WORKAROUND
-STUB = $(shell sh $(LOCAL_PATH)/../takeOutHeaderFiles.sh $(LOCAL_PATH)/../../../pEpEngine/ $(LOCAL_PATH)/../../)
+STUB = $(shell sh $(LOCAL_PATH)/../takeOutHeaderFiles.sh $(LOCAL_PATH)/../../../planckCoreV3/ $(LOCAL_PATH)/../../)
 $(info $(STUB))
 
 LIB_PEP_TRANSPORT_INCLUDE_FILES := $(wildcard $(LOCAL_PATH)/../../src/*.h*)
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../pEpEngine/build-android/include \
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../planckCoreV3/build-android/include \
 				$(LIB_PEP_TRANSPORT_INCLUDE_FILES:%=%)
 
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)../include
