3710fa5d3d8da968495a70a5cbafddb70cf4f0fd
ignaciogarcia <ig@pep.security>
2023-11-27 11:46:33 +0100

    CORE-230 Update path

--- build-android/jni/Android.mk
+++ build-android/jni/Android.mk
@@ -1,5 +1,6 @@
 $(warning ==== LIBPEPTRANSPORT android.mk START)
 LOCAL_PATH := $(call my-dir)
+PLANCK_CORE_PATH:=$(LOCAL_PATH)/../../../..
 
 
 include $(CLEAR_VARS)
@@ -11,12 +12,12 @@ LOCAL_MODULE    := pEpTransport
 LOCAL_CPPFLAGS += -std=c99
 
 #FIXME: WORKAROUND
-STUB = $(shell sh $(LOCAL_PATH)/../takeOutHeaderFiles.sh $(LOCAL_PATH)/../../../planckCoreV3/ $(LOCAL_PATH)/../../)
+STUB = $(shell sh $(LOCAL_PATH)/../takeOutHeaderFiles.sh $(PLANCK_CORE_PATH)/ $(LOCAL_PATH)/../../)
 $(info $(STUB))
 
 LIB_PEP_TRANSPORT_INCLUDE_FILES := $(wildcard $(LOCAL_PATH)/../../src/*.h*)
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../planckCoreV3/build-android/include \
+LOCAL_C_INCLUDES += $(PLANCK_CORE_PATH)/build-android/include \
 				$(LIB_PEP_TRANSPORT_INCLUDE_FILES:%=%)
 
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)../include
