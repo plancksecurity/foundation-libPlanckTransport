157ae1a985599c7e050401063dade7380dce7358
David Lanzendörfer <dl@planck.security>
2024-02-13 08:24:01 -0800

    Cleanup x64 build

--- build-windows/generate_code.cmd
+++ build-windows/generate_code.cmd
@@ -1,8 +1,6 @@
-:: The script is located in ...\pEpForWindowsAdapterSolution\libpEpTransport\build-windows\
-SET current_directory=%~dp0
+SET PY=%1\vcpkg\installed\x64-windows\tools\python3\python.exe
 
-:: YML2 directory is ...\pEpForWindowsAdapterSolution\yml2\
-SET yml2_directory=%current_directory%..\..\..\pEpForWindowsAdapterSolution\yml2
+SET yml2_directory=%1\yml2
 
 SET YML2PROC="%yml2_directory%\yml2proc"
 
@@ -10,27 +8,27 @@ SET YML2PROC="%yml2_directory%\yml2proc"
 PUSHD ..\src
 IF NOT EXIST generated MKDIR generated
 
-PY %YML2PROC% -E utf-8 -y gen_c.ysl2 transport_status_code.yml2 -o transport_status_code.h
+%PY% %YML2PROC% -E utf-8 -y gen_c.ysl2 transport_status_code.yml2 -o transport_status_code.h
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-PY %YML2PROC% -E utf-8 -y gen_strings.ysl2 transport_status_code.yml2 -o transport_status_code.c
+%PY% %YML2PROC% -E utf-8 -y gen_strings.ysl2 transport_status_code.yml2 -o transport_status_code.c
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-PY %YML2PROC% -E utf-8 -y gen_objc.ysl2 transport_status_code.yml2 -o PEPTransportStatusCode.h
+%PY% %YML2PROC% -E utf-8 -y gen_objc.ysl2 transport_status_code.yml2 -o PEPTransportStatusCode.h
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-PY %YML2PROC% -E utf-8 -y gen_kotlin.ysl2 transport_status_code.yml2 -o TransportStatusCode.kt
+%PY% %YML2PROC% -E utf-8 -y gen_kotlin.ysl2 transport_status_code.yml2 -o TransportStatusCode.kt
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-PY %YML2PROC% -E utf-8 -y gen_cs.ysl2 transport_status_code.yml2 -o TransportStatusCode.cs
+%PY% %YML2PROC% -E utf-8 -y gen_cs.ysl2 transport_status_code.yml2 -o TransportStatusCode.cs
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-PY %YML2PROC% -E utf-8 -P transport_status_code.yml2 -o transport_status_code.xml
+%PY% %YML2PROC% -E utf-8 -P transport_status_code.yml2 -o transport_status_code.xml
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-IF NOT EXIST ..\..\pEp MKDIR ..\..\pEp
-XCOPY /y *.c ..\..\pEp\
-XCOPY /y *.h ..\..\pEp\
+IF NOT EXIST %1\include\pEp MKDIR %1\include\pEp
+XCOPY /y *.c %1\include\pEp\
+XCOPY /y *.h %1\include\pEp\
 
 :end
 
