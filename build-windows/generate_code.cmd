b9c61c5ab662d1edfde5fe18769a810f0caa5165
David Lanzendörfer <dl@planck.security>
2024-08-21 15:52:11 -0700

    Move VCPKG to USERPROFILE

--- build-windows/generate_code.cmd
+++ build-windows/generate_code.cmd
@@ -1,27 +1,27 @@
 SET yml2_directory=%1\yml2
 
-SET YML2PROC="%yml2_directory%\yml2proc"
+SET YML2PROC=%PY% %PYTHON_SCRIPTS%\yml2proc
 
 :: Generate the code
 PUSHD ..\src
 IF NOT EXIST generated MKDIR generated
 
-%PY% %YML2PROC% -E utf-8 -y gen_c.ysl2 transport_status_code.yml2 -o transport_status_code.h
+%YML2PROC% -E utf-8 -y gen_c.ysl2 transport_status_code.yml2 -o transport_status_code.h
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-%PY% %YML2PROC% -E utf-8 -y gen_strings.ysl2 transport_status_code.yml2 -o transport_status_code.c
+%YML2PROC% -E utf-8 -y gen_strings.ysl2 transport_status_code.yml2 -o transport_status_code.c
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-%PY% %YML2PROC% -E utf-8 -y gen_objc.ysl2 transport_status_code.yml2 -o PEPTransportStatusCode.h
+%YML2PROC% -E utf-8 -y gen_objc.ysl2 transport_status_code.yml2 -o PEPTransportStatusCode.h
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-%PY% %YML2PROC% -E utf-8 -y gen_kotlin.ysl2 transport_status_code.yml2 -o TransportStatusCode.kt
+%YML2PROC% -E utf-8 -y gen_kotlin.ysl2 transport_status_code.yml2 -o TransportStatusCode.kt
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-%PY% %YML2PROC% -E utf-8 -y gen_cs.ysl2 transport_status_code.yml2 -o TransportStatusCode.cs
+%YML2PROC% -E utf-8 -y gen_cs.ysl2 transport_status_code.yml2 -o TransportStatusCode.cs
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
-%PY% %YML2PROC% -E utf-8 -P transport_status_code.yml2 -o transport_status_code.xml
+%YML2PROC% -E utf-8 -P transport_status_code.yml2 -o transport_status_code.xml
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
 IF NOT EXIST %1\include\pEp MKDIR %1\include\pEp
