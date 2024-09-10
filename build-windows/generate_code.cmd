b8dcc9f1010955978ef23aa3462fcde1ddd47efd
David Lanzendörfer <dl@planck.security>
2024-09-10 02:52:02 -0700

    Fix Python path discovery issue within VS2022

--- build-windows/generate_code.cmd
+++ build-windows/generate_code.cmd
@@ -1,3 +1,20 @@
+ECHO "Generate transport files"
+ECHO "PROCESSOR_ARCHITECTURE:%PROCESSOR_ARCHITECTURE%"
+ECHO "PROCESSOR_ARCHITEW6432:%PROCESSOR_ARCHITEW6432%"
+
+SET vcpkg_platform=x86-windows
+
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" SET vcpkg_platform=x64-windows
+IF "%PROCESSOR_ARCHITECTURE%"=="IA64" SET vcpkg_platform=x64-windows
+IF "%PROCESSOR_ARCHITECTURE%"=="ARM64" SET vcpkg_platform=arm64-windows
+
+IF "%PROCESSOR_ARCHITEW6432%"=="AMD64" SET vcpkg_platform=x64-windows
+IF "%PROCESSOR_ARCHITEW6432%"=="IA64" SET vcpkg_platform=x64-windows
+IF "%PROCESSOR_ARCHITEW6432%"=="ARM64" SET vcpkg_platform=arm64-windows
+
+SET PY=%USERPROFILE%\vcpkg\installed\%vcpkg_platform%\tools\python3\python.exe
+SET YML2PROC=%PY% %1\yml2\yml2proc
+
 :: Generate the code
 PUSHD ..\src
 IF NOT EXIST generated MKDIR generated
